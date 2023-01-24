c7c47242ba50048a951e88137aae66eaa17a4692
Dinuka Dissanayake <it21024818@my.sliit.lk>
2023-01-24 15:50:35 +0530

    commiits

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -14,6 +14,7 @@ export default (state = { isLoading: true, posts: [] }, action) => {
         numberOfPages: action.payload.numberOfPages,
       };
     case FETCH_BY_SEARCH:
+      return {  ...state, posts: action.payload };
     case FETCH_BY_CREATOR:
       return { ...state, posts: action.payload.data };
     case FETCH_POST:
